a605757a078c39103ccc8a6eaba65cff569ed5b3
Felix Wongso <wongso.felix@gmail.com>
2024-12-10 22:22:03 +0700

    Finish Login and Register, Create Routing

--- StellarFest/src/controllers/EventController.java
+++ StellarFest/src/controllers/EventController.java
@@ -4,12 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+import dao.EventDAO;
 import models.Event;
-import repositories.EventRepository;
 
 public class EventController {
-	//view event details keknya di views deh idk
-	public static void createEvent(String event_name, String event_date,String event_location,String event_description, String organizer_id ) {
+	private EventDAO eventDAO;
+	
+	public EventController() {
+		this.eventDAO = new EventDAO();
+	}
+
+	public void createEvent(String event_name, String event_date,String event_location,String event_description, String organizer_id ) {
 		if(!event_name.isBlank() && !event_date.isBlank() && !event_description.isBlank() && !organizer_id.isBlank() && !event_location.isBlank()) {
 			if(event_location.length() < 5 || event_description.length() > 200) {
 				return;
@@ -20,7 +25,7 @@ public class EventController {
 	            LocalDate inputDate = LocalDate.parse(event_date, formatter);
 	            LocalDate today = LocalDate.now();
 	            if (inputDate.isAfter(today)) {
-	                EventRepository.createEvent(event_name, event_date, event_location, event_description, organizer_id);
+	                eventDAO.createEvent(event_name, event_date, event_location, event_description, organizer_id);
 	            }
 	        } catch (DateTimeParseException e) {
 	            System.out.println("Invalid date format: " + e.getMessage());
@@ -29,10 +34,10 @@ public class EventController {
 			
 		}
 	}
-	public static Event getEventDetails(String event_id) {
+	public Event getEventDetails(String event_id) {
 		if(event_id.isBlank()) {
 			return null;
 		}
-		return EventRepository.getEventById(event_id);
+		return eventDAO.getEventById(event_id);
 	}
 }
--- StellarFest/src/factories/UserFactory.java
+++ StellarFest/src/factories/UserFactory.java
@@ -10,14 +10,16 @@ public class UserFactory {
 	public static User create(String user_id, String user_email, String user_name, String user_password, String user_role) {
 		if(user_role.toLowerCase().equals("admin")) {
 			return new Admin(user_id, user_email, user_name, user_password, user_role);
-		}else if (user_role.toLowerCase().equals("guest")) {
+		}
+		else if (user_role.toLowerCase().equals("guest")) {
 			return new Guest(user_id, user_email, user_name, user_password, user_role);
-		}else if (user_role.toLowerCase().equals("vendor")) {
+		}
+		else if (user_role.toLowerCase().equals("vendor")) {
 			return new Vendor(user_id, user_email, user_name, user_password, user_role);
-		}else if (user_role.toLowerCase().equals("eventorganizer")) {
+		}
+		else if (user_role.toLowerCase().equals("eventorganizer")) {
 			return new EventOrganizer(user_id, user_email, user_name, user_password, user_role);
 		}
 		return null;
 	}
-	
 }
